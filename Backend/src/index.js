d00c9d1c2e2860c37127abe87d5f1fbf4b8b03a5
Jagan <psbsvdmvcg@gmail.com>
2025-10-22 12:39:32 +0530

    added utilites and some data parsing middlewares

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,18 +6,35 @@ dotenv.config({
     path : './env'
 })
 
-
-
-
-
-
-
-
-
-
-
-
 connectDB()
+.then( ()=>{
+    app.on("error", (error) =>{
+        console.log("error : ",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port : ${process.env.PORT||8000}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ",err)
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
